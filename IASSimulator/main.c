c54d7a2a1ca3a2c152f1f816e7c856ba765d97c1
max.roecker <max.roecker@gmail.com>
2013-10-29 18:33:35 +0000

    First in first pau no cuuu

---
+++ IASSimulator/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: max
+ *
+ * Created on 29 de Outubro de 2013, 16:26
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
